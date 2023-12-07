0ddac9a9d59fbcf13a8fe647d2924885459d48bd
Sammy-OD <odunlamisamuel4@gmail.com>
2023-12-07 14:33:31 +0100

    listing page update

--- src/pages/SingleListing/index.tsx
+++ src/pages/SingleListing/index.tsx
@@ -12,13 +12,13 @@ const SingleListing = () => {
           <Navbar />
 
           {/* hero text */}
-          <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center'>
-            <h1 className='text-white text-6xl font-semibold mb-7'>Midas Furniture Inc.</h1>
+          <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center w-full'>
+            <h1 className='text-white text-2xl md:text-6xl font-semibold mb-7 text-center'>Midas Furniture Inc.</h1>
             <div className='flex gap-1.5 items-center'>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-white">
                 <path fillRule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
               </svg>
-              <p className='text-white'>Lagos, Nigeria</p>
+              <p className='text-white text-xs md:text-base'>Lagos, Nigeria</p>
             </div>
           </div>
         </div>
